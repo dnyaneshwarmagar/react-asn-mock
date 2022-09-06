f9d3005b9cdd2fb3772851bbd43c4ef1b2c501e0
dnyaneshwarmagar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2022-09-06 15:08:24 +0530

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,21 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import {Routes,Route} from "react-router-dom"
 import './App.css'
+import Home from './components/Home/Home'
+import Navbar from './components/Nav/Navbar'
+import Puppy from './components/Puppy.jsx/Puppy'
+import Search from './components/Search/Search'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/puppy" element={<Puppy/>}/>      
+        <Route path="/search" element={<Search/>}/>    
+      </Routes>
     </div>
   )
 }
