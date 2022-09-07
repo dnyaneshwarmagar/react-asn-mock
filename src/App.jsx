6e90d7cb4584ac240e75bb1a942ffb9a1fe2d840
dnyaneshwarmagar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2022-09-07 11:28:10 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
-import {  useState } from 'react'
-import {Routes,Route} from "react-router-dom"
-import './App.css'
-import Home from './components/Home/Home'
-import Navbar from './components/Nav/Navbar'
-import Puppy from './components/Puppy.jsx/Puppy'
-import Search from './components/Search/Search'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Nav/Navbar";
+import Puppy from "./components/Puppy.jsx/Puppy";
+import Search from "./components/Search/Search";
 
 function App() {
-  let [breed,setBreed]=useState("")
- 
+  let [breed, setBreed] = useState("");
+
   return (
     <div className="App">
       <div>
-
-      <Navbar/>
+        <Navbar />
       </div>
       <div>
-      <Routes>
-        <Route path="/" element={<Home setBreed={setBreed}/>}/>
-        <Route path="/puppy" element={<Puppy breed={breed}/>}/>      
-        <Route path="/search" element={<Search setBreed={setBreed}/> }/>    
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home setBreed={setBreed} />} />
+          <Route path="/puppy" element={<Puppy breed={breed} />} />
+          <Route path="/search" element={<Search setBreed={setBreed} />} />
+        </Routes>
       </div>
-     
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
